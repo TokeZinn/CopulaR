6c6d136b7a510eccde96b733549bef88a60fbabf
Toke Zinn <34207343+TokeZinn@users.noreply.github.com>
2020-09-08 09:54:48 +0200

    Update Interpolations
    Added mapping for index list  to values.

--- R/Interpolations.R
+++ R/Interpolations.R
@@ -64,8 +64,8 @@ setMethod("initialize", signature(.Object = "EquidistantPartition"), function(.O
   }
 
 
-  .Object@index <- approxfun(x = .Object@partition, y = seq(from = 1, to = .Object@n), method = "constant")
-  .Object@value <- approxfun(x = seq(from = 1, to = .Object@n), y = .Object@partition, method = "constant")
+  .Object@index <- approxfun(x = .Object@partition, y = seq(from = 1, to = .Object@n), yleft = NA, yright = NA, method = "constant")
+  .Object@value <- approxfun(x = seq(from = 1, to = .Object@n), y = .Object@partition, yleft = NA, yright = NA, method = "constant")
 
 
   .Object
@@ -84,6 +84,9 @@ setMethod("Index", signature(P = "Mesh"), function(x,..., P){
 
   res
 })
+setGeneric("Idx2Val", function(...,P){standardGeneric("Idx2Val")})
+setMethod("Idx2Val", signature(P = "Partition"), function(n,P){sapply(n, function(n){P@partition[n]})})
+setMethod("Idx2Val", signature(P = "Mesh"), function(n,P){lapply(1:length(n), function(i){Idx2Val(n[[i]],P =  P@mesh[[i]])})})
 setMethod("LongIndex", signature(P = "Mesh"), function(x,..., P){
 
   Points <- list(x,...)
@@ -99,14 +102,27 @@ setMethod("LongIndex", signature(P = "Mesh"), function(x,..., P){
 
   res
 })
-
-
 setGeneric("Value", function(n,.Object){standardGeneric("Value")})
 setMethod("Value", signature(n = "numeric", .Object = "Partition"), function(n,.Object){.Object@value(n)})
-setGeneric("Interval", function(x,.Object){standardGeneric("Interval")})
-setMethod("Interval", signature(x = "numeric", .Object = "Partition"), function(x,.Object){
-  indices = .Object@index(x)
-  return(cbind(indices, indices + 1))})
+setGeneric("Interval", function(x,...,P){standardGeneric("Interval")})
+setMethod("Interval", signature(x = "numeric", P = "Partition"), function(x,P){
+  n <- P@n
+  indices <- Index(x,P = P)
+  indices <- ifelse(indices == n, n-1, indices)
+  return(cbind(indices,indices + 1))
+  })
+setMethod("Interval", signature(x = "numeric", P = "Mesh"), function(x,...,P){
+  n <- lapply(P@mesh, function(m){m@n})
+  indices <- Index(x,..., P = P)
+  indices <- lapply(1:length(indices), function(i){
+
+    temp = ifelse(indices[[i]] == n[[i]], n[[i]] -1 , indices[[i]])
+    temp = cbind(temp, temp + 1)
+    temp = unname(temp)
+    return(temp)})
+
+  indices
+})
 setGeneric("MakeMesh", function(P, ...){standardGeneric("MakeMesh")})
 setMethod("MakeMesh", signature(P = "Partition"), function(P,...){
   M = Mesh()
@@ -133,29 +149,20 @@ setMethod("FillValues", signature(f = "function", M = "Mesh"), function(f,M,...)
 })
 setGeneric("LinearInterpolation", function(x, ..., M){standardGeneric("LinearInterpolation")})
 setMethod("LinearInterpolation", signature(x = "numeric", M = "Mesh" ), function(x, ..., M){
-
-
-
 })
+
+
 
 LinInt = function(x, x_1, x_0, f_1, f_0){
   alpha = (x-x_0)/(x_1-x_0)
   f_0*(1-alpha) + f_1*alpha
 }
 
+P = EquidistantPartition(a = 0, b = 1, n = 1000)
+M = MakeMesh(P,P,P)
+U =  seq(from = 0, to = 1, length.out = 1000)
 
-P = EquidistantPartition(a = 0, b = 1, n = 100)
-M = MakeMesh(P,P)
-
-Upper_Index = sapply(M@mesh, function(m){m@n})
-
-x = y = seq(from = 0, to = 1, length.out = 11)
-
-Index(x,y,P = M)
-
-
-
-
-
-
+microbenchmark::microbenchmark(Idx2Val(1:100,P = P))
+microbenchmark::microbenchmark(Interval(U,U,U, P = M))
+microbenchmark::microbenchmark(Idx2Val(Index(U,U,U, P = M), P = M))
 
